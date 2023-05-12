1ee6b0d8567f746afa400ca2595167370b6d5b56
ixiakoyama <mina.imi0509@gmail.com>
2023-05-12 12:23:03 +0900

    5/12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' =>['guest']], function(){
+    //ログインフォームの表示
+    Route::get('/',[AuthController::class,'showLogin'])->name('showLogin');
+    //ログイン処理
+    Route::post('login',[AuthController::class,'login'])->name('login');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+/*Auth::routes();
+//ログインフォームの表示
+    Route::get('/',[AuthController::class,'showLogin'])->name('showLogin');
+    //ログイン処理
+    Route::post('login',[AuthController::class,'login'])->name('login');
+    */
